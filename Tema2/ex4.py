ac9b50323516a8ad415b0823ad54750d6fdb55a7
Andrei G <andrei.grideanu@gmail.com>
2021-03-10 18:12:32 +0200

    Exercitii curs 2

--- Tema2/ex4.py
+++ Tema2/ex4.py
@@ -1,15 +1,16 @@
-x = 4 + 5j
-while x:
-    if isinstance(x, int):
-        x = x + 3
-        print(x)
-        break
-    elif isinstance(x, float):
-        x = x // 2
-        print(x)
-        break
-    elif isinstance(x, complex):
-        print("Numarul complex "+ str(complex(x))+ "\n" + "Valoare modul " + str(abs(x)))
-        break
-    else:
-        print("Insert numeric value")
+x = 4 + 9j
+
+if isinstance(x, int):
+    x = x + 3
+    print(x)
+
+elif isinstance(x, float):
+    x = x // 2
+    print(x)
+
+elif isinstance(x, complex):
+    print("Numarul complex "+ str(complex(x))+ "\n" + "Valoare modul " + str(abs(x)))
+
+else:
+    print("Insert numeric value")
+
